0ce19a6df13891f539be478bce31f0b1cb45c2bc
Mike Liu <mike.liu@nimbisservices.com>
2016-05-16 12:25:37 -0400

    Remove autogenerated files.

--- setup.py
+++ setup.py
@@ -4,29 +4,27 @@ from setuptools import find_packages, setup
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
 
-setup(
-    name = 'django-cards',
-    version = '0.1',
-    packages = find_packages(),
-    install_requires = [
-        'Django>=1.8, <1.9',
-        'requests',
-        'django-bootstrap3',
-        'libsass',
-        'django-sass-processor',
-    ],
-    include_package_data = True,
-    license = 'MIT License',
-    description = "A simple app to view trello's development board",
-    long_description = README,
-    url = 'https://github.com/mliudev/trello-card-viewer',
-    author = 'Mike Liu',
-    author_email = 'mike.liu@mliu.io',
-    classifiers = [
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Framework :: Django :: 1.8',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-    ],
-)
+setup(name='django-cards',
+      version='0.1',
+      packages=find_packages(),
+      install_requires=[
+          'Django>=1.8, <1.9',
+          'requests',
+          'django-bootstrap3',
+          'libsass',
+          'django-sass-processor',
+      ],
+      include_package_data=True,
+      license='MIT License',
+      description="A simple app to view trello's development board",
+      long_description=README,
+      url='https://github.com/mliudev/trello-card-viewer',
+      author='Mike Liu',
+      author_email='mike.liu@mliu.io',
+      classifiers=[
+          'Environment :: Web Environment',
+          'Framework :: Django',
+          'Framework :: Django :: 1.8',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.7',
+      ])
--- trello_cards/views.py
+++ trello_cards/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse
 from trello_cards.utils import APIParser
 
 
-# Create your views here.
 def home(request):
     parser = APIParser()
     trello_lists = parser.lists
